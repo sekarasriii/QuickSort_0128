b6848acd0804d7d415fa254c22e6720383c13287
sekarasriii <sekareeesh@gmail.com>
2024-03-29 09:42:03 +0700

    dengan fungsi void, menukar nilai element pada index x dengan element index y

--- Activity3_QuickSort/Activity3_QuickSort.cpp
+++ Activity3_QuickSort/Activity3_QuickSort.cpp
@@ -29,3 +29,11 @@ void input(){
 		cin >> arr[i];
 	}
 }
+//swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+	mov_count++;
+}
