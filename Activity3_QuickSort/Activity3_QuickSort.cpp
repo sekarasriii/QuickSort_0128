8194a68db8c549bd2fef35038489a919f8a07abb
sekarasriii <sekareeesh@gmail.com>
2024-03-29 09:34:01 +0700

    menggunakan fungsi void dengan memasukkan panjang element array

--- Activity3_QuickSort/Activity3_QuickSort.cpp
+++ Activity3_QuickSort/Activity3_QuickSort.cpp
@@ -7,3 +7,11 @@ int cmp_count = 0; //number of comparasion
 int mov_count = 0; //number of data movements
 int n;
 
+void input(){
+	while (true)
+	{
+		cout << "Masukan panjang element array:";
+		cin >> n;
+
+	}
+}
