172f0bc4d3b22fe0fb91d607a14cfde3e347d1f6
sekarasriii <sekareeesh@gmail.com>
2024-03-29 09:54:54 +0700

    dengan fungsi while mencari element yang lebih besar daripada pivot

--- Activity3_QuickSort/Activity3_QuickSort.cpp
+++ Activity3_QuickSort/Activity3_QuickSort.cpp
@@ -7,7 +7,7 @@ int cmp_count = 0; //number of comparasion
 int mov_count = 0; //number of data movements
 int n;
 
-void input(){
+void input() {
 	while (true)
 	{
 		cout << "Masukan panjang element array:";
@@ -44,5 +44,20 @@ void q_short(int low, int high)
 	int pivot, i, j;
 	if (low > high) {//step 1
 		return;
+	}
+
+	pivot = arr[low]; //step 2
+	i = low + 1;// step 3
+	j = high; //step 4
+
+	while (i <= j)// step 10
+	{
+		//search for an element greater than pivot
+		while ((arr[i]) <= pivot) && (i <= high)) //step 5
+		{
+			i++; //step 6
+			cmp_count++;
+		}
+
 	}
 }
